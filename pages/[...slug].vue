ef425feee4d41f42885247d771be0efb5e5110ab
Gerome <repo-booster@proton.me>
2024-11-28 11:11:29 +1100

    Create [...slug].vue

---
+++ pages/[...slug].vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <NuxtLink to="/">Go Back Home</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+console.log('Attempted to access:', route.path);
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin: 2rem auto;
+}
+</style>
